2d3144d30391f42f6f74b2086eba893826e11df4
Celine Junker <cjunker@c2r7p6.42nice.fr>
2022-04-21 16:53:17 +0200

    cappy

---
+++ cappy.sh
@@ -0,0 +1,74 @@
+test (){
+		clear
+		cat ~/framecappybebou/ff1
+		sleep 0.1	
+		clear
+		cat ~/framecappybebou/ff2
+		sleep 0.1
+		clear
+		cat ~/framecappybebou/ff3
+		sleep 0.1
+		clear
+		cat ~/framecappybebou/ff4
+		sleep 0.1
+		clear
+		cat ~/framecappybebou/ff5
+		sleep 0.1
+		clear
+		cat ~/framecappybebou/ff6
+		sleep 0.1
+		clear
+		cat ~/framecappybebou/ff7
+		sleep 0.1
+		clear
+		cat ~/framecappybebou/ff8
+		sleep 0.1
+		clear
+		cat ~/framecappybebou/ff9
+		sleep 0.1
+		clear
+		cat ~/framecappybebou/ff9r
+		sleep 0.1
+		clear
+		cat ~/framecappybebou/ff9v
+		sleep 0.1
+		clear
+		cat ~/framecappybebou/ff9l
+		sleep 0.1
+		clear
+		cat ~/framecappybebou/ff9g
+		sleep 0.1
+		clear
+		cat ~/framecappybebou/ff9m
+		sleep 0.1
+		clear
+		cat ~/framecappybebou/ff9
+		sleep 0.1
+		clear
+		cat ~/framecappybebou/ff8
+		sleep 0.1
+		clear
+		cat ~/framecappybebou/ff7
+		sleep 0.1
+		clear
+		cat ~/framecappybebou/ff6
+		sleep 0.1
+		clear
+		cat ~/framecappybebou/ff5
+		sleep 0.1
+		clear
+		cat ~/framecappybebou/ff4
+		sleep 0.1
+		clear
+		cat ~/framecappybebou/ff3
+		sleep 0.1
+		clear
+		cat ~/framecappybebou/ff2
+		sleep 0.1
+		clear
+		cat ~/framecappybebou/ff1
+		sleep 0.1
+		test
+	}
+
+test 
